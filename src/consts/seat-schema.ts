49a9a079b354be14fc503236e13e9b1faaf9e42b
JoezerG <joezergullo@gmail.com>
2024-04-23 21:38:32 -0500

    seats data

---
+++ src/consts/seat-schema.ts
@@ -0,0 +1,27 @@
+export const SeatsZonesFloor_1 = [
+  { id: "Z-1", name: "1", columns: 5, rows: 4, seats: 20, floor: 1 },
+  { id: "Z-2", name: "2", columns: 4, rows: 8, seats: 32, floor: 1 },
+  { id: "Z-3", name: "3", columns: 8, rows: 6, seats: 42, floor: 1 },
+  { id: "Z-4", name: "4", columns: 8, rows: 5, seats: 40, floor: 1 },
+  { id: "Z-5", name: "5", columns: 8, rows: 6, seats: 42, floor: 1 },
+  { id: "Z-6", name: "6", columns: 5, rows: 8, seats: 40, floor: 1 },
+  { id: "Z-7", name: "7", columns: 6, rows: 4, seats: 24, floor: 1 },
+  { id: "Z-8", name: "8", columns: 5, rows: 8, seats: 40, floor: 1 },
+  { id: "Z-9", name: "9", columns: 5, rows: 4, seats: 20, floor: 1 },
+  { id: "Z-10", name: "10", columns: 8, rows: 6, seats: 46, floor: 1 },
+  { id: "Z-11", name: "11", columns: 10, rows: 5, seats: 48, floor: 1 },
+  { id: "Z-12", name: "12", columns: 10, rows: 3, seats: 30, floor: 1 },
+  { id: "Z-13", name: "13", columns: 8, rows: 2, seats: 16, floor: 1 },
+  { id: "Z-14", name: "14", columns: 12, rows: 4, seats: 45, floor: 1 },
+  { id: "Z-15", name: "15", columns: 8, rows: 4, seats: 30, floor: 1 },
+];
+
+export const SeatsZonesFloor_2 = [
+  { id: "Z-16", name: "16", columns: 8, rows: 5, seats: 40, floor: 2 },
+  { id: "Z-17", name: "17", columns: 8, rows: 5, seats: 40, floor: 2 },
+  { id: "Z-18", name: "18", columns: 8, rows: 5, seats: 40, floor: 2 },
+  { id: "Z-19", name: "19", columns: 8, rows: 5, seats: 40, floor: 2 },
+  { id: "Z-20", name: "20", columns: 4, rows: 4, seats: 13, floor: 2 },
+  { id: "Z-21", name: "21", columns: 4, rows: 5, seats: 20, floor: 2 },
+  { id: "Z-22", name: "22", columns: 8, rows: 5, seats: 40, floor: 2 },
+];
